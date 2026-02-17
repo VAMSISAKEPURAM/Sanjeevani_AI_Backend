870fa896d83586ae4849688e872b93660d395824
Vamsi Sakepuram <vamsisakepuram@gmail.com>
2026-02-17 22:56:52 +0530

    updated_cros

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import uvicorn
 from app.routes import router
 
 app = FastAPI()
 
+# CORS Middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "https://sanjeevani-ai-frontend-2lu9poyn4-vamsis-projects-fc511db6.vercel.app"
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Include API routes
 app.include_router(router)
 
 if __name__ == "__main__":
